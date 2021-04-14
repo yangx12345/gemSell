1d65201f7fc6e402ba6e68b2c6963e7685a309e8
yangxiao <erlsan@163.com>
2021-04-14 22:36:49 +0800

    修改sql文件

--- gemSell/src/main/resources/sql/gemSell.sql
+++ gemSell/src/main/resources/sql/gemSell.sql
@@ -1,47 +1,45 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : yang
  Source Server Type    : MySQL
  Source Server Version : 50728
  Source Host           : localhost:3306
- Source Schema         : carmaintain
+ Source Schema         : gemsell
 
  Target Server Type    : MySQL
  Target Server Version : 50728
  File Encoding         : 65001
 
- Date: 14/04/2021 17:38:30
+ Date: 14/04/2021 22:36:04
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for user_info
+-- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info`  (
-                              `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
-                              `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-                              `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-                              `role` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色 0系统管理员 1管理员 2员工',
-                              `name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
-                              `departId` int(11) NOT NULL COMMENT '员工部门Id',
-                              `gender` char(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '男' COMMENT '性别',
-                              `birthday` date NULL DEFAULT NULL COMMENT '生日',
-                              `phone` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号码',
-                              `email` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱地址',
-                              `address` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
-                              `idcard` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '身份证号码',
-                              `workstatus` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '工作状态 0正常 1已出工',
-                              `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
-                              PRIMARY KEY (`user_id`) USING BTREE,
-                              UNIQUE INDEX `uqe_username`(`username`) USING BTREE
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+                         `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+                         `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+                         `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+                         `role` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色 0管理员 1用户',
+                         `name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
+                         `gender` char(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '男' COMMENT '性别',
+                         `birthday` date NULL DEFAULT NULL COMMENT '生日',
+                         `phone` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号码',
+                         `email` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱地址',
+                         `address` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
+                         `idcard` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '身份证号码',
+                         `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+                         PRIMARY KEY (`user_id`) USING BTREE,
+                         UNIQUE INDEX `uqe_username`(`username`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user_info
+-- Records of user
 -- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
